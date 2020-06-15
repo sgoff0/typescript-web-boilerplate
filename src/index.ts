c2c8197eec73a578b639e6a24ac0a81f8828f941
Steven Goff <51921135+sgoff0@users.noreply.github.com>
2020-06-15 10:16:15 -0500

    Web boilerplate

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
 import './utils/env';
-import logger from './utils/logger'
-logger.info("Hello world")
+import logger from './utils/logger';
+import './utils/env';
+
+import { createServer } from 'http';
+import { app } from './app';
+
+const port = process.env.PORT || 5000;
+
+(async () => {
+    createServer(app)
+        .listen(
+            port,
+            () => logger.info(`Server running on port ${port}`)
+        );
+})();
